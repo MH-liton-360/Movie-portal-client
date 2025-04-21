9045fb670e0fd9a400e5349c7ddf27c6c3298543
MH-liton-360 <liton72524nk@gmail.com>
2025-04-21 15:54:39 +0600

    Design and authentication seventy percent Done

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -2,18 +2,18 @@ import { useEffect, useState } from "react";
 import Poster from "./poster";
 const Main = () => {
 
-    const [posters, setAdventures] = useState([]);
+    const [posters, setPosters] = useState([]);
 
     useEffect(() => {
         fetch('MoviePortalBlogs.json')
             .then((res) => res.json())
-            .then((data) => setAdventures(data));
+            .then((data) => setPosters(data));
 
     }, [])
 
     return (
         <div>
-            <h2 className="text-3xl font-bold py-10">All Movies Information</h2>
+            <h2 className="text-3xl font-bold py-10">All Movies Features</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {
                     posters.map(poster => <Poster poster={poster} key={poster.id}></Poster>)
